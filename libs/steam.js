b82c04d15b91c6784edc3db30821146e3aaa7bd8
VLADOS776 <kurtukovvlad@gmail.com>
2016-11-13 15:58:24 +0300

    Private account fix

--- libs/steam.js
+++ libs/steam.js
@@ -19,17 +19,21 @@ function profile(id) {
 
 function profileToString(profile) {
     var mostPlayedGames = "";
-    for (var i = 0; i < profile.mostPlayedGames[0].mostPlayedGame.length; i++) {
-        var game = profile.mostPlayedGames[0].mostPlayedGame[i];
-        mostPlayedGames += `<tr><td>${game.gameName}</td><td>${game.hoursOnRecord}</td></tr>`;
+    if (typeof profile.mostPlayedGames != 'undefined') {
+        mostPlayedGames = "<table><tr><th>Game</th><th>Hours</th></tr>";
+        for (var i = 0; i < profile.mostPlayedGames[0].mostPlayedGame.length; i++) {
+            var game = profile.mostPlayedGames[0].mostPlayedGame[i];
+            mostPlayedGames += `<tr><td>${game.gameName}</td><td>${game.hoursOnRecord}</td></tr>`;
+        }
+        mostPlayedGames += '</table>';
+    } else {
+        mostPlayedGames = 'Not found';
     }
     var stat = profile.stateMessage[0].replace('<br/>', " ");
     var vac = profile.vacBanned != 0 ? ' | VAC: '+profile.vacBanned : "";
     var tamp = `<img src="${profile.avatarFull[0]}" style="width: 80px; height: 80px; border-radius: 0;"><br>    
-                <b>${profile.steamID}</b> [${stat}]${vac}<br>Most played games:
-                <table><tr><th>Game</th><th>Hours</th></tr>
-                ${mostPlayedGames}
-                </table>`
+                <b>${profile.steamID}</b> [${stat}]${vac}<br>Most played games:             
+                ${mostPlayedGames}`
     return tamp
 }
 
